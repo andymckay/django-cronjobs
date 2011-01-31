1a0a8965a4842796cd19a6d40b921f2756b37e0f
Andy McKay <amckay@mozilla.com>
2011-01-31 06:39:51 -0800

    remembering how pypi works

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.1'
+readme = open(os.path.join(os.path.dirname(__file__), "readme.rst"))
+long_description = readme.read()
 
-setup(name='cronjobs',
+setup(name='django-cronjobs',
       version=version,
       description="Cronjob helper for Django",
-      long_description=open("readme.rst").read() + "\n",
+      long_description=long_description,
       classifiers=[
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
